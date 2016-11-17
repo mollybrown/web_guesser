89a2ddd35e6207208f612d9f84ad69f91be7dc77
Molly Brown <mollbrown@gmail.com>
2016-11-16 23:59:58 -0700

    added guess counter logic

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,34 +2,56 @@ require 'sinatra'
 require 'sinatra/reloader'
 
   set :number, rand(100)
+  @@guesses_remaining = 6
 
   get '/' do
     guess = params['guess'].to_i
+    guesses_remaining = guess_counter(guess)
     message, color = check_guess(guess)
     erb :index, :locals => {:number => settings.number,
                             :message => message,
-                            :color => color}
+                            :color => color,
+                            :guesses_remaining => @@guesses_remaining}
+  end
+
+  def guess_counter(guess)
+    @@guesses_remaining -= 1
+  end
+
+  def game_win
+    @@guesses_remaining = 5
+    settings.number = rand(100)
+    message = "You won! A new number has been generated."
+  end
+
+  def game_lose
+    @@guesses_remaining = 5
+    settings.number = rand(100)
+    message = "You lose! A new number has been generated."
   end
 
   def check_guess(guess)
-    if guess.zero?
-      message = "Please enter a valid guess..."
-      color = "ffffff"
-    elsif guess == settings.number
-      message = "You got it right!"
-      color = "#008000"
-    elsif guess > (settings.number + 5)
-      message = "Way too high!"
-      color = "#FF0000"
-    elsif guess > settings.number
-      message = "Too high!"
-      color = "#FE8484"
-    elsif guess < (settings.number - 5)
-      message = "Way too low!"
-      color = "#FF0000"
-    elsif guess < settings.number
-      message = "Too low!"
-      color = "#FE8484"
+    unless @@guesses_remaining == 0
+      if guess.zero?
+        message = "Please enter a valid guess..."
+        color = "ffffff"
+      elsif guess == settings.number
+        message = game_win
+        color = "#008000"
+      elsif guess > (settings.number + 5)
+        message = "Way too high!"
+        color = "#FF0000"
+      elsif guess > settings.number
+        message = "Too high!"
+        color = "#FE8484"
+      elsif guess < (settings.number - 5)
+        message = "Way too low!"
+        color = "#FF0000"
+      elsif guess < settings.number
+        message = "Too low!"
+        color = "#FE8484"
+      end
+      return message, color
     end
-    return message, color
+    game_lose
   end
